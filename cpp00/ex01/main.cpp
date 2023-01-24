9e82472babfefbc2e0f340fbf1660145468ce10c
CMS37 <rhtn037@naver.com>
2023-01-25 07:06:19 +0900

    .

--- cpp00/ex01/main.cpp
+++ cpp00/ex01/main.cpp
@@ -1,13 +1,32 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   PhoneBook.cpp                                      :+:      :+:    :+:   */
+/*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: min-cho <min-cho@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/01/25 07:05:56 by min-cho           #+#    #+#             */
-/*   Updated: 2023/01/25 07:06:04 by min-cho          ###   ########.fr       */
+/*   Created: 2023/01/24 22:53:34 by min-cho           #+#    #+#             */
+/*   Updated: 2023/01/25 07:06:09 by min-cho          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-//make class
+#include <iostream>
+#include "Account.hpp"
+
+int	main(void)
+{
+	std::string cmd;
+	
+	while (1)
+	{
+		std::cout << "PhoneBook > ";
+		if (!(std::getline(std::cin, cmd)))
+			return (1);
+		if (cmd == "ADD")
+			;
+		else if (cmd == "SERCH")
+			;
+		else if (cmd == "EXIT")
+			;
+	}	
+}
